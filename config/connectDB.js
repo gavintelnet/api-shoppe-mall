03d8df64a27268b0fd3251d432bc8a575f7944a6
TajoDag <taodx.dev@gmail.com>
2024-06-18 15:07:20 +0700

    fix be timout

--- config/connectDB.js
+++ config/connectDB.js
@@ -1,17 +1,56 @@
+// const mongoose = require('mongoose');
+
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect(process.env.DB_URL, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true
+//     });
+//     console.log('MongoDB Connected');
+//   } catch (err) {
+//     console.error('MongoDB Connection Failed', err.message);
+//     // Exit process with failure
+//     process.exit(1);
+//   }
+// };
+
+// module.exports = connectDB;
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.DB_URL, {
       useNewUrlParser: true,
-      useUnifiedTopology: true
+      useUnifiedTopology: true,
+      maxPoolSize: 10, // Thay vì poolSize, sử dụng maxPoolSize
+      serverSelectionTimeoutMS: 5000,
+      socketTimeoutMS: 45000,
+      family: 4
     });
-    console.log('MongoDB Connected');
+
+    mongoose.connection.on('connected', () => {
+      console.log('MongoDB Connected');
+    });
+
+    mongoose.connection.on('error', (err) => {
+      console.error('MongoDB Connection Error:', err.message);
+    });
+
+    mongoose.connection.on('disconnected', () => {
+      console.log('MongoDB Disconnected');
+    });
+
+    process.on('SIGINT', async () => {
+      await mongoose.connection.close();
+      console.log('MongoDB Connection Closed');
+      process.exit(0);
+    });
+
   } catch (err) {
     console.error('MongoDB Connection Failed', err.message);
-    // Exit process with failure
     process.exit(1);
   }
 };
 
 module.exports = connectDB;
+
